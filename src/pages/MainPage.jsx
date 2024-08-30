64d2e43080dac7a119ebc52a599580d8741df5b0
dev-rjw <rjw0105@gmail.com>
2024-08-30 17:08:32 +0900

    move : Main.jsx -> MainPage.jsx로 변경

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InputWindow from "../components/InputWindow";
+import DocumentList from "../components/DocumentList";
+
+function MainPage() {
+    return (
+        <>
+            <InputWindow />
+            <DocumentList />
+        </>
+    );
+}
+
+export default MainPage;
