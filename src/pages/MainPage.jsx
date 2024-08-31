62a4826c35df22975d70dacef112a1caa03403db
dev-rjw <rjw0105@gmail.com>
2024-08-31 22:43:26 +0900

    feat : 회원/비회원 별 InputWindow 표시 여부 체크

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InputWindow from "../components/InputWindow";
 import DocumentList from "../components/DocumentList";
+import { supabase } from "../supabase";
 
 function MainPage() {
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        getUserData();
+    });
+
+    const getUserData = async () => {
+        const { data } = await supabase.auth.getUser();
+        setUserData(data);
+        return data;
+    };
+
     return (
         <>
-            <InputWindow />
+            {userData.user !== null ? <InputWindow /> : ""}
             <DocumentList />
         </>
     );
