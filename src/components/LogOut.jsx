6736a1e0538426a9f680f60ad587dc3e0a1fcb34
leeyeram84 <killhvhl1129@naver.com>
2024-08-31 22:50:56 +0900

    fix : 로그인 여부 확인 로직 삭제

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -2,19 +2,8 @@ import React, { useState } from "react";
 import { supabase } from "../supabase";
 
 const LogOut = () => {
-    const [signIn, setSignIn] = useState(false);
-
-    // 로그인 유효성 검사
-    async function checkSignIn() {
-        const session = await supabase.auth.getSession();
-        const isSignIn = !!session.data.session;
-
-        setSignIn(isSignIn);
-    }
-
     async function signOut() {
         await supabase.auth.signOut();
-        checkSignIn();
     }
 
     return (
