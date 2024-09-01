4d6147a0ca2b1fb65f15d2beaa6b629919cf539e
leeyeram84 <killhvhl1129@naver.com>
2024-09-01 19:25:23 +0900

    feat : 회원가입시 공란 유효성 검사 로직 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -23,9 +23,20 @@ function SignUp() {
     const SignUp = async (e) => {
         e.preventDefault();
 
-        // 유효성 검사
-
-        // 부합하지 않으면 (경고) => 함수도 종료 return
+        // input 공백 유효성 검사
+        if (!email) {
+            alert("이메일을 입력해주세요");
+            return;
+        } else if (!password) {
+            alert("비밀번호를 입력해주세요");
+            return;
+        } else if (password.length < 6) {
+            alert("6자리 이상의 비밀 번호를 설정해주세요");
+            return;
+        } else if (!nickName) {
+            alert("닉네임을 입력해주세요");
+            return;
+        } // 프로필 사진은 필수 입력값이 아니기때문에 프로필을 제외하기 위해 else를 쓰지 않음);
 
         const { data, error } = await supabase.auth.signUp({
             email: email,
@@ -33,7 +44,7 @@ function SignUp() {
             options: {
                 data: {
                     nickName: nickName,
-                    profileUrl: null
+                    profileUrl: setProfileUrl
                 }
             }
         });
@@ -43,18 +54,16 @@ function SignUp() {
     // 프로필
     async function checkProfile() {
         const { data: userData } = await supabase.auth.getUser();
-        console.log(userData);
 
         const userProfileUrl = userData.user.user_metadata.profileUrl;
         //옵셔널체이닝
 
         const { data } = supabase.storage.from("avatars").getPublicUrl(userProfileUrl ?? "default-profile.jpg");
-        // nullish 병합 연산자 '??' 값이 null 이면 ?? 오른쪽
 
         setProfileUrl(data.publicUrl);
     }
 
-    // // 프로필 사진 변경
+    // 프로필 사진 변경
     async function handleFileInputChange(files) {
         const [file] = files;
 
@@ -67,9 +76,6 @@ function SignUp() {
 
         // data.path = 프로필 이미지 넘버링
         setProfileUrl(`https://yhqidmepyhxhostsyirn.supabase.co/storage/v1/object/public/avatars/${data.path}`);
-
-        // 회원가입 성공 시 로그인 페이지 이동
-        // navigate()
     }
 
     return (
