3204088d5647b3bf078d0aeb2da50531550c5e52
김태흔 <you@example.com>
2024-09-02 10:19:09 +0900

    feat/마이페이지 내가쓴 카드 보여주기

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,13 +2,19 @@ import styled from "styled-components";
 import MyModal from "./../components/MyModal";
 import { useContext } from "react";
 import { PostsContext } from "../App";
+import { useSearchParams } from "react-router-dom";
+import DocumentCard from "../components/DocumentCard";
 
 const Dashboard = styled.div`
     background-color: greenyellow;
 `;
 
-function MyPage() {
+function MyPage({ intro }) {
     const { posts, user } = useContext(PostsContext);
+    const [searchParams] = useSearchParams();
+    const userId = searchParams.get("id");
+
+    const filterdPosts = posts.filter((post) => post.user_id === userId);
 
     return (
         <>
@@ -19,9 +25,18 @@ function MyPage() {
                 <h2>이메일</h2>
                 <p>{user?.user.email}</p>
                 <h2>자기소개</h2>
-                {/* <p>{intro}</p> */}
+                <p>{intro ? user?.user.user_metadata.intro : "자기소개를 등록해주세요"}</p>
                 <MyModal />
             </Dashboard>
+            <ul>
+                {filterdPosts.map((post) => {
+                    return (
+                        <li key={post.id}>
+                            <DocumentCard post={post} />;
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 }
